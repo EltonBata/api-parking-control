4162563c39e5cc9b17ec3f235dcad86367bdfdfd
EltonBata <eltonbata14@gmail.com>
2023-12-06 11:11:26 +0200

    fix: remove a non jakarta validation rule

--- src/main/java/com/api/parkingcontrol/dtos/ParkingSpotDto.java
+++ src/main/java/com/api/parkingcontrol/dtos/ParkingSpotDto.java
@@ -1,16 +1,14 @@
 package com.api.parkingcontrol.dtos;
 
-import org.hibernate.validator.constraints.UniqueElements;
-
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record ParkingSpotDto(
-                @NotBlank @UniqueElements String parkingSpotNumber,
-                @NotBlank @Size(max = 7) String responsibleName,
+                @NotBlank @Size(max = 7) String parkingSpotNumber,
+                @NotBlank String responsibleName,
                 @NotBlank String apartment,
                 @NotBlank String block,
-                @NotBlank @UniqueElements String licencePlateCar,
+                @NotBlank String licencePlateCar,
                 @NotBlank String brandCar,
                 @NotBlank String modelCar,
                 @NotBlank String colorCar) {
